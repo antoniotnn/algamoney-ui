83829c2b6ad6241e39cd6f74ad1dc39d2530c50f
Antonio Pires <tonan@outlook.com.br>
2024-10-21 07:41:29 -0300

    Decoding JWT and storing in Local Storage

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -1,3 +1,4 @@
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -7,23 +8,46 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   oauthTokenUrl = 'http://localhost:8080/oauth/token';
+  jwtPayload: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private jwtHelper: JwtHelperService
+  ) {
+    this.carregarToken();
+  }
 
   login(usuario: string, senha: string): Promise<void> {
     const headers = new HttpHeaders()
       .append('Content-Type', 'application/x-www-form-urlencoded')
-      .append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA=='); // angular:@ngul@r0
+      .append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA==');
 
     const body = `username=${usuario}&password=${senha}&grant_type=password`;
 
     return this.http.post(this.oauthTokenUrl, body, { headers })
       .toPromise()
-      .then(response => {
+      .then((response: any) => {
         console.log(response);
+        this.armazenarToken(response['access_token']);
       })
       .catch(response => {
         console.log(response);
       });
   }
+
+  public armazenarToken(token: string) {
+    this.jwtPayload = this.jwtHelper.decodeToken(token);
+    console.log(this.jwtPayload);
+
+    localStorage.setItem('token', token);
+  }
+
+  public carregarToken() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.armazenarToken(token);
+    }
+  }
+
 }
