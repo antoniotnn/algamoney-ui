d195c90aa2e89dbce6e2146f054a341a0b975022
Antonio Pires <tonan@outlook.com.br>
2024-10-27 09:10:36 -0300

    Displaying the PDF to the user

--- src/app/relatorios/relatorio-lancamentos/relatorio-lancamentos.component.ts
+++ src/app/relatorios/relatorio-lancamentos/relatorio-lancamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RelatoriosService } from './../relatorios.service';
 
 @Component({
   selector: 'app-relatorio-lancamentos',
@@ -10,13 +11,15 @@ export class RelatorioLancamentosComponent implements OnInit {
   periodoInicio?: Date;
   periodoFim?: Date;
 
-  constructor() { }
+  constructor(private relatoriosService: RelatoriosService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   gerar() {
-    console.log(this.periodoInicio);
-    console.log(this.periodoFim);
+    this.relatoriosService.relatorioLancamentosPorPessoa(this.periodoInicio!, this.periodoFim!)
+      .then(relatorio => {
+        const url = window.URL.createObjectURL(relatorio);
+        window.open(url);
+      });
   }
 }
