d1e1737ea6ed120410f73ce6796f537bcd413faf
Antonio Pires <tonan@outlook.com.br>
2024-10-30 07:25:26 -0300

    changing url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'http://localhost:8080',
   tokenAllowedDomains: [/localhost:8080/],
   tokenDisallowedRoutes: [/\/oauth2\/token/],
-  oauthCallbackUrl: 'http://tnn.development.com:8000/authorized'
+  oauthCallbackUrl: 'http://local-algamoney.com:8000/authorized'
 };
